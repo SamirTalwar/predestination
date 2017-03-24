0d6987635604c6bafc30a4f02c30d92cc3ec5d7e
Samir Talwar <samir@noodlesandwich.com>
2017-03-24 08:55:19 +0000

    If no input file is provided, generate a random game.

--- src/cli.py
+++ src/cli.py
@@ -4,6 +4,7 @@ from life import Life
 
 import curses
 import numpy
+import random
 import sys
 import time
 
@@ -12,13 +13,11 @@ class CLI:
     inputs = {'.': 0, 'x': 1}
     outputs = {0: '∙', 1: '█'}
 
-    def __init__(self, input_file):
-        with open(input_file) as f:
-            self.life = Life(numpy.matrix([
-                [CLI.inputs[c] for c in line.strip()]
-                for line in f]))
+    def __init__(self, input_file=None):
+        self.input_file = input_file
 
     def run(self, stdscr):
+        self.load(stdscr)
         try:
             while True:
                 self.display(stdscr)
@@ -26,6 +25,21 @@ class CLI:
                 self.life.next()
         except KeyboardInterrupt:
             pass
+
+    def load(self, stdscr):
+        if self.input_file:
+            with open(self.input_file) as f:
+                self.life = Life(numpy.matrix([
+                    [CLI.inputs[c] for c in line.strip()]
+                    for line in f]))
+        else:
+            height, width = stdscr.getmaxyx()
+            height -= 1
+            width -= 1
+            k = height * width
+            self.life = Life(
+                numpy.matrix(random.choices([0, 1], k=k))
+                .reshape((height, width)))
 
     def display(self, stdscr):
         stdscr.clear()
@@ -35,4 +49,4 @@ class CLI:
 
 
 if __name__ == '__main__':
-    curses.wrapper(CLI(sys.argv[1]).run)
+    curses.wrapper(CLI(*sys.argv[1:]).run)
