c347c87e6bc5c2b37c24c8423980373b51c3a77f
Samir Talwar <samir@noodlesandwich.com>
2017-03-27 00:12:45 +0100

    Factor out different methods for different modes.

--- src/cli.py
+++ src/cli.py
@@ -15,40 +15,68 @@ class CLI:
 
     def __init__(self, input_file=None):
         self.input_file = input_file
-        self.playing = True
 
     def run(self, stdscr):
+        CLIRunner(self.input_file, stdscr).run()
+
+
+class CLIRunner:
+    def __init__(self, input_file, stdscr):
+        self.input_file = input_file
+        self.stdscr = stdscr
+
+    def run(self):
         curses.noecho()
         curses.cbreak()
-        stdscr.nodelay(True)
 
-        self.load(stdscr)
+        self.load()
+
+        mode = self.live
         try:
             while True:
-                if self.playing:
-                    self.display(stdscr)
-                    self.life.next()
-                time.sleep(0.1)
-                try:
-                    ch = stdscr.getkey()
-                except:
-                    ch = None
-
-                if ch == 'q':
-                    return
-                elif ch == ' ':
-                    self.playing = not self.playing
-        except KeyboardInterrupt:
+                mode = mode()
+        except (KeyboardInterrupt, Quit):
             pass
 
-    def load(self, stdscr):
+    def live(self):
+        self.stdscr.nodelay(True)
+        self.display()
+        self.life.next()
+        time.sleep(0.1)
+        try:
+            ch = self.stdscr.getkey()
+        except:
+            ch = None
+
+        if ch == 'q':
+            return self.quit
+        elif ch == ' ':
+            return self.pause
+        else:
+            return self.live
+
+    def pause(self):
+        self.stdscr.nodelay(False)
+        self.display()
+        ch = self.stdscr.getkey()
+        if ch == 'q':
+            return self.quit
+        elif ch == ' ':
+            return self.live
+        else:
+            return self.pause
+
+    def quit(self):
+        raise Quit()
+
+    def load(self):
         if self.input_file:
             with open(self.input_file) as f:
                 self.life = Life(numpy.matrix([
                     [CLI.inputs[c] for c in line.strip()]
                     for line in f]))
         else:
-            height, width = stdscr.getmaxyx()
+            height, width = self.stdscr.getmaxyx()
             height -= 1
             width -= 1
             k = height * width
@@ -56,11 +84,15 @@ class CLI:
                 numpy.matrix(random.choices([0, 1], k=k))
                 .reshape((height, width)))
 
-    def display(self, stdscr):
-        stdscr.clear()
+    def display(self):
+        self.stdscr.clear()
         for i, line in enumerate(self.life.matrix.tolist()):
-            stdscr.addstr(i, 0, ''.join(CLI.outputs[n] for n in line))
-        stdscr.refresh()
+            self.stdscr.addstr(i, 0, ''.join(CLI.outputs[n] for n in line))
+        self.stdscr.refresh()
+
+
+class Quit(Exception):
+    pass
 
 
 if __name__ == '__main__':
