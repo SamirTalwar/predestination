a1642cca602ca1e42b87be5002c0f6fde9e1fb85
Samir Talwar <samir@noodlesandwich.com>
2017-11-27 15:34:34 +0000

    CLI: Remember where the marker is.

--- src/cli.py
+++ src/cli.py
@@ -27,7 +27,7 @@ class CLIRunner:
         self.input_file = options.input_file
         self.style = options.style
         self.stdscr = stdscr
-        self.marker = None
+        self.show_marker = False
 
     def run(self):
         curses.noecho()
@@ -49,7 +49,7 @@ class CLIRunner:
 
     def live(self):
         self.stdscr.nodelay(True)
-        self.marker = None
+        self.show_marker = False
 
         self.display()
         self.life = self.life.next(self.style)
@@ -61,8 +61,7 @@ class CLIRunner:
     def pause(self):
         self.stdscr.nodelay(False)
 
-        if not self.marker:
-            self.marker = (int(self.width / 2), int(self.height / 2))
+        self.show_marker = True
         self.display()
 
         ch = self.read()
@@ -95,6 +94,7 @@ class CLIRunner:
             height, width = self.stdscr.getmaxyx()
             self.width = width
             self.height = height
+            self.marker = (int(self.width / 2), int(self.height / 2))
             self.life = Life.random(height - 1, width - 1)
 
     def display(self):
@@ -102,7 +102,7 @@ class CLIRunner:
         for i, line in enumerate(self.life.matrix.tolist()):
             self.stdscr.addstr(i, 0, ''.join(CLI.outputs[n] for n in line),
                                curses.color_pair(1))
-        if self.marker:
+        if self.show_marker:
             self.stdscr.addstr(self.marker[1], self.marker[0], ' ',
                                curses.color_pair(2))
         self.stdscr.move(self.height - 1, 0)
